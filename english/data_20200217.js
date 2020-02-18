e5166728e4dd5fcdf88eb514970c760fcfd4338b
elian <elian0211@gmail.com>
2020-02-18 14:22:52 +0800

    english

---
+++ english/data_20200217.js
@@ -0,0 +1,34 @@
+[
+    {
+    title: "bear",
+    src: "datas/20200217/bear.jpg"
+    },
+    {
+    title: "bird",
+    src: "datas/20200217/bird.jpg"
+    },
+    {
+    title: "desert",
+    src: "datas/20200217/desert.jpg"
+    },
+    {
+    title: "fox",
+    src: "datas/20200217/fox.jpg"
+    },
+    {
+    title: "mountains",
+    src: "datas/20200217/mountains.jpg"
+    },
+    {
+    title: "rain forest",
+    src: "datas/20200217/rain_forest.jpg"
+    },
+    {
+    title: "snow and ice",
+    src: "datas/20200217/snow_and_ice.jpg"
+    },
+    {
+    title: "tiger",
+    src: "datas/20200217/tiger.jpg"
+    }
+    ]
